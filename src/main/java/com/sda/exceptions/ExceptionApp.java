92c1763f2ab771dfa2cd81b997bb0334e89c07d6
Bluetouch <bogusz28@wp.pl>
2020-05-24 11:36:22 +0200

    try&catch&catch

--- src/main/java/com/sda/exceptions/ExceptionApp.java
+++ src/main/java/com/sda/exceptions/ExceptionApp.java
@@ -2,6 +2,7 @@ package com.sda.exceptions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ExceptionApp {
     public static void main(String[] args) {
@@ -18,15 +19,24 @@ public class ExceptionApp {
 //        List<String> teksty = null;
 //        teksty.add("T1");
         try {
-            List<String> teksts = new ArrayList<>();
-            teksts.add("Ala");
-            teksts.add("ma");
-            teksts.add("kota");
+            List<String> texts = List.of("Ala", "ma", "kota", "a", "Karol", "komputer");
 
-            teksts.get(5);
-            System.out.println("This line is not executed!");
-        }catch (IndexOutOfBoundsException exception){
-            System.out.println("Error occurred!");
+            System.out.println("Choose index from 0 to " + texts.size() +": ");
+            Scanner scanner = new Scanner(System.in);
+            String text = scanner.nextLine();
+            System.out.println("You entered: " + text);
+
+            int index = Integer.parseInt(text);
+            System.out.println("Chosen text: " + texts.get(index));
+
+//            teksts.get(5);
+//            System.out.println("This line is not executed!");
+        } catch (IndexOutOfBoundsException exception) {
+            System.out.println("You asked for too much!");
+        } catch (NumberFormatException exception) {
+            System.out.println("Text you rntered is not a number!");
+        }finally {
+            System.out.println("Exiting!");
         }
     }
 }
