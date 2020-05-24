f723a5ba683b38a57727fb2ccbc1fcfb1df2aa8d
Bluetouch <bogusz28@wp.pl>
2020-05-24 10:58:32 +0200

    exceptions1

---
+++ src/main/java/com/sda/exceptions/ExceptionApp.java
@@ -0,0 +1,23 @@
+package com.sda.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExceptionApp {
+    public static void main(String[] args) {
+////        IndexOutOfBoundsException
+//        List<String> teksts = new ArrayList<>();
+//        teksts.add("T1");
+//        teksts.add("T2");
+//
+//        teksts.get(3);
+
+
+////        NullPointerException
+
+//        List<String> teksty = null;
+//        teksty.add("T1");
+
+
+    }
+}
